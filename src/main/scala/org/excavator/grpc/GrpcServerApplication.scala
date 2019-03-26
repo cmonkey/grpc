5ed7906630674fae6bcdb15cba4c6cfec7f8c23c
CMonkey <42.codemonkey@gmail.com>
2019-03-26 13:54:49 +0800

    refator GrpcServerApplication

--- src/main/scala/org/excavator/grpc/GrpcServerApplication.scala
+++ src/main/scala/org/excavator/grpc/GrpcServerApplication.scala
@@ -1,20 +1,22 @@
 package org.excavator.grpc
 
+import java.util.concurrent.TimeUnit
+
 import io.grpc.{Server, ServerBuilder}
 
 class GrpcServerApplication {
 
-  def start() = {
+  def start(port: Int) = {
 
     val p = new ProductServiceRpc()
 
-    val server = ServerBuilder.forPort(53000)
+    val server = ServerBuilder.forPort(port)
       .addService(p)
       .build()
       .start()
 
     addHook(server)
-    server.awaitTermination()
+    server.awaitTermination(10, TimeUnit.SECONDS)
   }
 
   private def addHook(server: Server) = {
